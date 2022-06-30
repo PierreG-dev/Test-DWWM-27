a01f285331c53f7b131c341d6cad2b82278c867f
Pierre <35926163+PierreG-dev@users.noreply.github.com>
2022-06-30 11:24:31 +0200

    Added data.js & index.html

---
+++ data.js
@@ -0,0 +1,97 @@
+let ENtext = `
+<h1>
+The blob, this "almost immortal" creature, which Thomas will study
+Pesquet and 350,000 students
+</h1>
+<p id="subtitle">
+The blob, an unclassifiable organism at the heart of the fascinations of biologists,
+flies to the International Space Station on Tuesday.
+</p>
+<div id="img-container">
+<div>
+  <span>Published on 08/10/2021 17:21</span
+  ><span>⏱ Reading time:<strong>3 min.</strong></span>
+</div>
+<figure>
+  <img
+    src="https://www.francetvinfo.fr/pictures/pHWhhJ8EL031fTUPtbjGIm1M-ek/0x317:6090x3743/944x531/filters:format(webp)/2021/08/10/phpoKG5NU.jpg"
+    alt=""
+  />
+  <figcaption>
+  A blob installed on a branch of the Vincennes zoo in Paris, on the 24th
+  October 2019. (ALEXIS SCIARD / MAXPPP)
+  </figcaption>
+</figure>
+</div>
+<div id="text-container">
+<div>
+  <p>
+  They will be the stars of a joint study in the
+  stars by <u>Thomas Pesquet</u> and at the same time on earth
+  by several thousand children. Blobs must board at
+  middle of nearly 3,000 kilos of on-board equipment and a batch of
+  experience projects in a supply cargo ship, heading for the
+  International Space Station.
+  </p>
+  <blockquote>
+    <p>
+    "To define it, we often start by saying what it is not,
+    it is neither a plant, nor an animal, nor a fungus."
+    </p>
+    <span
+      ><strong>Audrey Dussutour, researcher at the CNRS in Toulouse</strong>
+      <i>at franceinfo</i>
+    </span>
+  </blockquote>
+</div>
+</div>
+`;
+
+let FRtext = `
+<h1>
+Le blob, cette créature "quasi immortelle", que vont étudier Thomas
+Pesquet et 350 000 élèves
+</h1>
+<p id="subtitle">
+Le blob, organisme inclassable au cœur des fascinations des biologistes,
+s'envole mardi vers la Station spatiale internationale.
+</p>
+<div id="img-container">
+<div>
+  <span>Publié le 10/08/2021 17:21</span
+  ><span>⏱ Temps de lecture : <strong>3 min.</strong></span>
+</div>
+<figure>
+  <img
+    src="https://www.francetvinfo.fr/pictures/pHWhhJ8EL031fTUPtbjGIm1M-ek/0x317:6090x3743/944x531/filters:format(webp)/2021/08/10/phpoKG5NU.jpg"
+    alt=""
+  />
+  <figcaption>
+    Un blob installé sur une branche du zoo de Vincennes à Paris, le 24
+    octobre 2019. (ALEXIS SCIARD / MAXPPP)
+  </figcaption>
+</figure>
+</div>
+<div id="text-container">
+<div>
+  <p>
+    Ils seront les stars d'une étude conjointement menée dans les
+    étoiles par <u>Thomas Pesquet</u> et dans le même temps sur terre
+    par plusieurs milliers d'enfants. Les blobs doivent embarquer au
+    milieu de près de 3 000 kilos d'équipement de bord et d'un lot de
+    projets d'expérience dans un cargo de ravitaillement, direction la
+    Station spatiale internationale.
+  </p>
+  <blockquote>
+    <p>
+      "Pour le définir, on commence souvent par dire ce qu’il n’est pas,
+      il n’est ni une plante, ni un animal, ni un champignon."
+    </p>
+    <span
+      ><strong>Audrey Dussutour, chercheuse au CNRS à Toulouse</strong>
+      <i>à franceinfo</i>
+    </span>
+  </blockquote>
+</div>
+</div>
+`;
